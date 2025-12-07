d2f5c600aae3f02edfb0b826da13b0a56a22c87d
saanvishetty393 <saanvishetty393@gmail.com>
2025-12-07 14:46:43 +0530

    Update script.js

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,6 @@
 
 let box = document.getElementById("age");
-let button = document.getElementById("info-bnt");
+let button = document.getElementById("info-btn");
 button.addEventListener("click",() => {
     alert("First year computer science engineer");
 })
